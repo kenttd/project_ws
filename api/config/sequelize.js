0245dafb81172422d6998bf28291c20d7d69fa9f
Kent Travis Donovan <kenttravisdonovan@gmail.com>
2024-05-10 17:12:49 +0700

    test5 pg

--- api/config/sequelize.js
+++ api/config/sequelize.js
@@ -1,6 +1,9 @@
+import pg from "pg";
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 const uri = `postgres://${process.env.SUPABASE_USER}:${process.env.SUPABASE_PASSWORD}@${process.env.SUPABASE_HOST}:${process.env.SUPABASE_PORT}/${process.env.SUPABASE_DB}`;
-const sequelize = new Sequelize(uri);
+const sequelize = new Sequelize(uri, {
+  dialectModule: pg,
+});
 
 module.exports = sequelize;
