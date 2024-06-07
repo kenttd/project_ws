ecca7acefb784e7177b1b932074304c4b8767aad
Ken <ken3lliot@gmail.com>
2024-06-07 20:38:41 +0700

    fixing

--- migration/migrations/20240607130342-create-access-log.js
+++ migration/migrations/20240607130342-create-access-log.js
@@ -22,6 +22,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('access_logs');
+    await queryInterface.dropTable('access_log');
   }
 };
