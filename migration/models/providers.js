ab16146275df338cb284c1489a526577e3088dba
Rafi Putra <111548948+rafisuryawan@users.noreply.github.com>
2024-06-18 13:58:55 +0700

    provider,token,model

--- migration/models/providers.js
+++ migration/models/providers.js
@@ -1,23 +1,45 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class providers extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  providers.init({
-    name: DataTypes.STRING
-  }, {
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/sequelize");
+
+class Providers extends Model {}
+Providers.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    hospital_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    department: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
     sequelize,
-    modelName: 'providers',
-  });
-  return providers;
-};
+    modelName: "Providers",
+    tableName: "providers",
+    timestamps: false,
+    name: {
+      singular: "Provider",
+      plural: "Providers",
+    },
+  }
+);
+
+module.exports = Providers;
