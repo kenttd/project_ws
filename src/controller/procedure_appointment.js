2b50c7cb230a8584f6c3e0c104a0dfaaa3a9c324
Sony-Arc <133092308+Sony-Arc@users.noreply.github.com>
2024-06-19 10:21:56 +0700

    addProcedureAppointment controller

--- src/controller/procedure_appointment.js
+++ src/controller/procedure_appointment.js
@@ -8,6 +8,38 @@ module.exports = {
   getProcedureAppointment: async function (req, res) {
     const procedureArray = req.body.procedureArray;
   },
+  addProcedureAppointment: async function (req, res) {
+    const procedureArray = req.body.procedureArray;
+    const appointment_id = req.body.appointment_id;
+
+    if(procedureArray.length === 0) {
+      return res.status(400).json({
+        message: "Procedure array is empty"
+      });
+    }
+
+    if(Array.isArray(procedureArray) === false) {
+      const procedure = await procedureAppointment.create({
+        appointment_id: appointment_id,
+        procedure_id: procedureArray,
+      })
+
+      return res.status(201).json({
+        message: "Procedure appointment added"
+      });
+    }
+
+    for (let i = 0; i < procedureArray.length; i++) {
+      const procedure = await procedureAppointment.create({
+        appointment_id: appointment_id,
+        procedure_id: procedureArray[i],
+      })
+    }
+
+    return res.status(201).json({
+      message: "Procedure appointment added"
+    });
+  },
   getProcedureAppointment: async function (req, res) {},
   deleteProcedureAppointment: async function (req, res) {},
 };
